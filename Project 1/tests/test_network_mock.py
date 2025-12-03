dcecc69614ecf98a92c5da59389e847ed70ba6a5
Dimplegurajala <dimplegurajala@gmail.com>
2025-12-03 17:42:50 +0530

    Refine comment in test_simulate_image_failure
    Updated comment to clarify test purpose regarding high traffic resilience.

--- Project 1/tests/test_network_mock.py
+++ Project 1/tests/test_network_mock.py
@@ -4,11 +4,11 @@ from playwright.sync_api import expect
 def test_simulate_image_failure(page_context):
     page = page_context
     
-    # NETWORK MOCKING: Block all images to simulate CDN crash
+    # NETWORK MOCKING: Block all images to simulate crash
     page.route("**/*.jpg", lambda route: route.abort())
 
     login = LoginPage(page)
     login.do_login("standard_user", "secret_sauce")
 
-    # Assert page loads even if images are broken (Resilience)
+    # Assert page loads even if images are broken- when there is high traffic- we need the app to respond not crash
     expect(page.locator(".title")).to_have_text("Products")
