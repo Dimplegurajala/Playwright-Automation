bee4051a96434688f4a9df957579509003ec2d8d
Dimplegurajala <dimplegurajala@gmail.com>
2025-12-03 17:41:30 +0530

    Refactor login test description and formatting
    Updated the test description for clarity.

--- Project 1/tests/test_login_scenarios.py
+++ Project 1/tests/test_login_scenarios.py
@@ -2,7 +2,7 @@ import pytest
 from pages.login_page import LoginPage
 from playwright.sync_api import expect
 
-# DATA-DRIVEN TESTING (Standard vs Locked User)
+# DATA-DRIVEN TESTING - with various login details (Standard vs Locked User)
 @pytest.mark.parametrize("username, password, expected_url", [
     ("standard_user", "secret_sauce", "inventory.html"),
     ("locked_out_user", "secret_sauce", "https://www.saucedemo.com/"), 
@@ -13,5 +13,4 @@ def test_login_data_driven(page_context, username, password, expected_url):
 
     login.do_login(username, password)
 
-    # Assert URL based on user type
     expect(page).to_have_url(f"**/{expected_url}")
